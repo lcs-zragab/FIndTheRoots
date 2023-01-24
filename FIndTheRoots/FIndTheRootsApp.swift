27c7d1be18049d05c656647f2a2bf86e421f6610
zaid ragab <zragab@lcs.on.ca>
2023-01-24 08:50:56 -0500

    Initial Commit

---
+++ FIndTheRoots/FIndTheRootsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIndTheRootsApp.swift
+//  FIndTheRoots
+//
+//  Created by Zaid Ragab on 2023-01-24.
+//
+
+import SwiftUI
+
+@main
+struct FIndTheRootsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
